6b6e130b14371ae35f1cd1efaddb1255dd47ff53
Nightcomputer <muhammadramdan2401@gmail.com>
2025-03-14 09:03:19 +0800

    menambahkan fitur keranjang

--- index.php
+++ index.php
@@ -58,13 +58,13 @@
     <!-- KERANJANG -->
     <div class="iconkeranjang">
         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="red" class="bi bi-basket2-fill" viewBox="0 0 16 16">
-            <a href="#"></a>
+            <a href=""></a>
         <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1"/>
         </svg>
     </div>
 
 
-    
+
     <section class="container mt-4" id="makanan">
         <h1 class="text-center">MAKANAN</h1>
         <div class="row row-cols-2 row-cols-sm-2 row-cols-md-2 row-cols-lg-2 g-5" id="menu-makanan">
